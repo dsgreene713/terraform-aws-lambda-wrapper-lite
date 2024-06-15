d96e7b1d68b8ee77460a32d1c5d2d860b4b4a0e1
Dan Greene <dsgreene713@gmail.com>
2024-06-15 15:31:34 -0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,76 @@
+data "aws_partition" "current" {}
+
+locals {
+  function_output_path = "${path.module}/${var.function_name}.zip"
+}
+
+######################################################################################
+# iam
+######################################################################################
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "this" {
+  name               = "${var.function_name}-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+######################################################################################
+# lambda
+######################################################################################
+data "archive_file" "this" {
+  type        = "zip"
+  source_dir  = var.lambda_source_dir
+  output_path = local.function_output_path
+}
+
+resource "aws_lambda_function" "this" {
+  filename         = filebase64sha256(local.function_output_path)
+  function_name    = var.function_name
+  role             = aws_iam_role.this.arn
+  handler          = var.function_handler
+  runtime          = var.function_runtime
+  source_code_hash = data.archive_file.this.output_base64sha256
+  layers           = var.layers
+  timeout          = var.timeout
+  publish          = var.publish
+  kms_key_arn      = var.kms_key_arn
+
+  dynamic "environment" {
+    for_each = length(keys(var.environment_variables)) == 0 ? [] : [true]
+
+    content {
+      variables = var.environment_variables
+    }
+  }
+
+  dynamic "vpc_config" {
+    for_each = var.vpc_subnet_ids != null && var.vpc_security_group_ids != null ? [true] : []
+
+    content {
+      security_group_ids = var.vpc_security_group_ids
+      subnet_ids         = var.vpc_subnet_ids
+    }
+  }
+
+  dynamic "logging_config" {
+    for_each = data.aws_partition.current.partition == "aws" ? [true] : []
+
+    content {
+      log_group             = var.logging_log_group
+      log_format            = var.logging_log_format
+      application_log_level = var.logging_log_format == "Text" ? null : var.logging_application_log_level
+      system_log_level      = var.logging_log_format == "Text" ? null : var.logging_system_log_level
+    }
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,96 @@
+######################################################################################
+# required variables
+######################################################################################
+variable "function_name" {
+  type        = string
+  description = "A unique name for your Lambda Function"
+}
+
+variable "lambda_source_dir" {
+  type        = string
+  description = "source directory for function code"
+}
+
+variable "function_handler" {
+  type        = string
+  description = "Lambda Function entrypoint in your code"
+}
+
+variable "function_runtime" {
+  type        = string
+  description = "Lambda Function runtime"
+}
+
+######################################################################################
+# optional variables
+######################################################################################
+variable "environment_variables" {
+  description = "A map that defines environment variables for the Lambda Function."
+  type        = map(string)
+  default     = {}
+}
+
+variable "tracing_mode" {
+  description = "Tracing mode of the Lambda Function. Valid value can be either PassThrough or Active."
+  type        = string
+  default     = "Active"
+}
+
+variable "logging_log_format" {
+  description = "The log format of the Lambda Function. Valid values are \"JSON\" or \"Text\"."
+  type        = string
+  default     = "Text"
+}
+
+variable "logging_application_log_level" {
+  description = "The application log level of the Lambda Function. Valid values are \"TRACE\", \"DEBUG\", \"INFO\", \"WARN\", \"ERROR\", or \"FATAL\"."
+  type        = string
+  default     = "INFO"
+}
+
+variable "logging_system_log_level" {
+  description = "The system log level of the Lambda Function. Valid values are \"DEBUG\", \"INFO\", or \"WARN\"."
+  type        = string
+  default     = "INFO"
+}
+
+variable "logging_log_group" {
+  description = "The CloudWatch log group to send logs to."
+  type        = string
+  default     = null
+}
+
+variable "vpc_subnet_ids" {
+  description = "List of subnet ids when Lambda Function should run in the VPC. Usually private or intra subnets."
+  type        = list(string)
+  default     = null
+}
+variable "vpc_security_group_ids" {
+  description = "List of security group ids when Lambda Function should run in the VPC."
+  type        = list(string)
+  default     = null
+}
+
+variable "layers" {
+  description = "List of Lambda Layer Version ARNs (maximum of 5) to attach to your Lambda Function."
+  type        = list(string)
+  default     = null
+}
+
+variable "kms_key_arn" {
+  description = "The ARN of KMS key to use by your Lambda Function"
+  type        = string
+  default     = null
+}
+
+variable "publish" {
+  description = "Whether to publish creation/change as new Lambda Function Version."
+  type        = bool
+  default     = false
+}
+
+variable "timeout" {
+  description = "The amount of time your Lambda Function has to run in seconds."
+  type        = number
+  default     = 120
+}
